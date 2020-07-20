828bafef82f58a35a91793fd684dbbc4abd1e4a0
jonnathanmoreno2000 <65380631+jonnathanmoreno2000@users.noreply.github.com>
2020-07-20 11:00:14 -0400

    Add files via upload

---
+++ prueba 2 login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css"     href="ESTILO/ESTILO.CSS">
+    
+    <title>Esta Es MI Tienda</title>
+</head>
+
+
+<body>
+<div id="contenedor">
+        <header>
+            <nav><h1>Mi Tienda</h1></nav>
+        </header>
+
+</body>
+</html>
